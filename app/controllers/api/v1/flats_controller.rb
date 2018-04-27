b555fe46e5ed9d70a158e97f8a1b53c7b2107676
Chikara Okada <chikara@gmail.com>
2018-04-27 15:03:15 +0900

    Flats.where lat and lng within mapBounds implemented; have not used %

--- app/controllers/api/v1/flats_controller.rb
+++ app/controllers/api/v1/flats_controller.rb
@@ -1,11 +1,27 @@
 class Api::V1::FlatsController < ApplicationController
+  before_action :load_flat, only: :show
+
   def index
-    @flats = Flat.all.includes(:images)
-    flats_serializer = parse_json @flats
-    json_response "Indexed flats succesfully", true, {flats: flats_serializer}, :ok
+    if params[:east] && params[:west] && params[:north] && params[:south]
+      p params[:east].to_f
+      p params[:west].to_f
+      p params[:north].to_f
+      p params[:south].to_f
+
+      @flats = Flat.where('lat < (?) AND lat > (?) AND lng < (?) AND lng > (?)', params[:north].to_f, params[:south].to_f, params[:east].to_f, params[:west].to_f).includes(:images)
+      flats_serializer = parse_json @flats
+      json_response "Indexed flats within area successfully", true, {flats: flats_serializer}, :ok
+    else
+      @flats = Flat.all
+      @flats = Flat.all.includes(:images)
+      flats_serializer = parse_json @flats
+      json_response "Indexed flats successfully", true, {flats: flats_serializer}, :ok
+    end
   end
 
   def show
+    flat_serializer = parse_json @flat
+    json_response "Showed flat successfully", true, {flat: flat_serializer}, :ok
   end
 
   def new
@@ -25,7 +41,16 @@ class Api::V1::FlatsController < ApplicationController
 
   private
 
+  def load_flat
+    @flat = Flat.find_by id: params[:id]
+    unless @flat.present?
+      json_response "Cannot find flat", false, {}, :not_found
+    end
+  end
+
   def strong_params
-    params.require(:flat).permit
+    params.require(:flat).permit(:user_id, :lat, :lng, :address1, :city, :zip, :country, :area,
+    :price_per_day, :price_per_month, :guests, :sales_point, :description,
+    :rooms, :beds, :flat_type, :bath)
   end
 end
