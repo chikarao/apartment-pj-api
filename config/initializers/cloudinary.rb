79ce0273d7d72e9bd4d5194fe6f7e01330c49b54
Chikara Okada <chikara@gmail.com>
2018-05-21 15:17:14 +0900

    Redoing last commit to implementing cloudinary upload;

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'chikarao'
-  config.api_key = '527588889181175'
-  config.api_secret = 'eJMnC5URXNV2HFHhGLqSAgFhJQM'
-  config.secure = true
-  config.cdn_subdomain = true
-end
