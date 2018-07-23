2900bb08f48ca5688a32c3cd77303a939cdaf680
Chikara Okada <chikara@gmail.com>
2018-07-23 18:16:30 +0900

    Flats joined with Amenity in flats_controller#index to filter based on amenity boolean search params values.

--- app/controllers/api/v1/flats_controller.rb
+++ app/controllers/api/v1/flats_controller.rb
@@ -11,49 +11,68 @@ class Api::V1::FlatsController < ApplicationController
       # p params[:west].to_f
       # p params[:north].to_f
       # p params[:south].to_f
-      p "!!!!!! price max: " + params[:price_max].to_s
-      p "!!!!!!! price min:" + params[:price_min].to_s
-      p "!!!!!!! bedrooms_min:" + params[:bedrooms_min].to_s
-      p "!!!!!!! bedrooms_max:" + params[:bedrooms_max].to_s
-      p "!!!!!!! bedrooms_exact:" + params[:bedrooms_exact].to_s
-      p "!!!!!!! size_max:" + params[:size_min].to_s
-      p "!!!!!!! size_max:" + params[:size_max].to_s
-      p "!!!!!!! station_min:" + params[:station_min].to_s
-      p "!!!!!!! station_max:" + params[:station_max].to_s
+      # p "!!!!!! price max: " + params[:price_max].to_s
+      # p "!!!!!!! price min:" + params[:price_min].to_s
+      # p "!!!!!!! bedrooms_min:" + params[:bedrooms_min].to_s
+      # p "!!!!!!! bedrooms_max:" + params[:bedrooms_max].to_s
+      # p "!!!!!!! bedrooms_exact:" + params[:bedrooms_exact].to_s
+      # p "!!!!!!! size_max:" + params[:size_min].to_s
+      # p "!!!!!!! size_max:" + params[:size_max].to_s
+      # p "!!!!!!! station_min:" + params[:station_min].to_s
+      # p "!!!!!!! station_max:" + params[:station_max].to_s
 
       # Base conditions for search; string
-      conditions = 'lat < (?) AND lat > (?) AND lng < (?) AND lng > (?) AND
+      base_conditions = 'lat < (?) AND lat > (?) AND lng < (?) AND lng > (?) AND
                     price_per_month >= (?) AND price_per_month <= (?) AND
                     size >= (?) AND size <= (?) AND
                     minutes_to_station >= (?) AND minutes_to_station <= (?)'
       # conditions = 'lat < (?) AND lat > (?) AND lng < (?) AND lng > (?) AND
       #               price_per_month > (?) AND price_per_month < (?) AND
       #               rooms > (?) AND rooms < (?)'
-      params_array = [conditions, params[:north].to_f, params[:south].to_f, params[:east].to_f, params[:west].to_f,
+      params_array = [base_conditions , params[:north].to_f, params[:south].to_f, params[:east].to_f, params[:west].to_f,
         params[:price_min].to_f,  params[:price_max].to_f,
         params[:size_min].to_f,  params[:size_max].to_f,
         params[:station_min].to_f,  params[:station_max].to_f]
 
       if params[:bedrooms_exact]
-        conditions.concat(' AND rooms = (?)')
+        base_conditions.concat(' AND rooms = (?)')
         # params[:bedrooms_min].to_i, params[:bedrooms_max].to_i]
         params_array.push(params[:bedrooms_exact].to_i)
       else
-        conditions.concat(' AND rooms >= (?)')
-        conditions.concat(' AND rooms <= (?)')
+        base_conditions.concat(' AND rooms >= (?)')
+        base_conditions.concat(' AND rooms <= (?)')
         # params_array = [conditions, params[:north].to_f, params[:south].to_f, params[:east].to_f, params[:west].to_f,
         #   params[:price_min].to_f,  params[:price_max].to_f]
         params_array.push(params[:bedrooms_min].to_i)
         params_array.push(params[:bedrooms_max].to_i)
       end
-      p "!!!!!!! conditions:" + conditions.to_s
-      p "!!!!!!! params_array:" + params_array.to_s
+
+      amenity_columns = Amenity.column_names
+      amenity_columns.each do |a|
+        params_string_value = params[a].to_s
+        # params_string_value = params["#{a}"].to_s
+        p "!!!!!!! in each params params_string_value:" + params_string_value.to_s
+        if params_string_value == "true"
+          p "!!!!!!! in each params amenity:" + a.to_s
+          base_conditions.concat(" AND #{a} = (?)")
+          params_value = true
+          params_array.push(params_value)
+        end
+      end
+
+      # p "!!!!!!! params base_conditions:" + base_conditions.to_s
+      # p "!!!!!!! params_array:" + params_array.to_s
+      # p "!!!!!!! amenity column_names: " + Amenity.column_names.to_s
 
       # parameters_for_conditions = params[:north].to_f, params[:south].to_f, params[:east].to_f, params[:west].to_f,
       #                             params[:price_min].to_f,  params[:price_max].to_f,
       #                             params[:bedrooms_min].to_i, params[:bedrooms_max].to_i, params[:bedrooms_exact].to_i
 
-      @flats = Flat.where(params_array).includes(:images)
+      # @flats = Flat.where(params_array).includes(:images)
+      # @flats = Flat.where(params_array)
+      # @flats = Flat.joins(:amenity).where('auto_lock = (?) OR flat_id = (?)', true, 138)
+      @flats = Flat.joins(:amenity).where(params_array)
+      # @flats = Flat.all
       # @flats = Flat.where('lat < (?) AND lat > (?) AND lng < (?) AND lng > (?) AND
       #                       price_per_month > (?) AND price_per_month < (?) AND
       #                       rooms > (?) AND rooms < (?) AND rooms = (?)',
