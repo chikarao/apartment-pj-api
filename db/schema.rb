7853da5396d4180ca417a92c20bb847120721d00
Chikara Okada <chikara@gmail.com>
2018-08-04 18:42:27 +0900

    STRIPE; added stripe_customer_id to users. Tried to remove swipe_customer id but need to spend time taking out dependents so that the column can be removed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180804093433) do
+ActiveRecord::Schema.define(version: 20180804094016) do
 
   create_table "amenities", force: :cascade do |t|
     t.integer "flat_id"
@@ -270,6 +270,7 @@ ActiveRecord::Schema.define(version: 20180804093433) do
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
     t.string "swipe_customer_id"
+    t.string "stripe_customer_id"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
