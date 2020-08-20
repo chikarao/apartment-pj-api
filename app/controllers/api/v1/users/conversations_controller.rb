8787d8755e05381921dcdc178736e2b94c6d6ad5
Chikara Okada <chikara@gmail.com>
2020-08-20 13:23:11 +0900

    Users/ConversationController; Fixed bug in get_set_status_of_conversation_users. Able to return user status of each user in user conversations (flat.user_id and conversation.user_id).

--- app/controllers/api/v1/users/conversations_controller.rb
+++ app/controllers/api/v1/users/conversations_controller.rb
@@ -84,24 +84,25 @@ class Api::V1::Users::ConversationsController < ApplicationController
       # reference: https://stackoverflow.com/questions/28954500/activerecord-where-field-array-of-possible-values
       # @conversations = Conversation.where(user_id: @user.id, deleted: false).or(Conversation.where(flat_id: flat_id_array, deleted: false)).includes(:messages, :user, :flat)
       # @conversations = Conversation.where(user_id: @user.id).or(Conversation.where(flat_id: flat_id_array)).includes(:messages, :user, :flat)
-      @conversations = Conversation.where(user_id: @user.id).or(Conversation.where(flat_id: flat_id_array))
-      p 'in users, ConversationsController, conversations_by_user, conversations, conversations.user_id: ' + @conversations.to_s + ' ' + @conversations[0].user_id.to_s
+      conversations = Conversation.where(user_id: @user.id).or(Conversation.where(flat_id: flat_id_array))
+      p 'in users, ConversationsController, conversations_by_user, conversations, conversations.user_id: ' + conversations.to_s + ' ' + conversations[0].user_id.to_s
 
       # p @conversation
-      if @conversations
+      if conversations
         if $redis
-          user_status_hash_array = get_status_of_conversation_users(@user.id, @conversations)
-          if user_status_hash_array.blank?
-            p '**********in users, conversations_by_user, user_status_hash_array, user_status_hash_array.blank?: ' + user_status_hash_array.to_s + ' ' +  user_status_hash_array.blank?.to_s
-            result_set = set_status_of_conversation_users(@user.id, @conversations)
-            if result_set
-              user_status_hash_array = get_status_of_conversation_users(@user.id, @conversations)
-            end
-          end
+          user_status_hash_array = get_set_status_of_conversation_users(@user.id, conversations)
+          # if user_status_hash_array.blank?
+          #   p '**********in users, conversations_by_user, user_status_hash_array, user_status_hash_array.blank?: ' + user_status_hash_array.to_s + ' ' +  user_status_hash_array.blank?.to_s
+          #   result_set = set_status_of_conversation_users(@user.id, conversations)
+          #   if result_set
+          #     user_status_hash_array = get_set_status_of_conversation_users(@user.id, conversations)
+          #   end
+          # end
           # p '**********in users, conversations_by_user, user_status_hash_array, user_status_hash_array.blank?: ' + user_status_hash_array.to_s + ' ' +  user_status_hash_array.blank?.to_s
         end
-        conversations_serializer = parse_json @conversations
-        json_response "Fetched conversations by user successfully", true, {conversations: conversations_serializer, other_user_status: user_status_hash_array}, :ok
+        conversations_serializer = parse_json conversations
+        p 'in users, ConversationsController, conversations_by_user, conversations, conversations.user_id last: ' + conversations.to_s + ' ' + conversations[0].user_id.to_s
+        json_response "Fetched conversations by user successfully", true, {conversations: conversations_serializer, other_user_status: user_status_hash_array, test_conversations: conversations.to_json}, :ok
       else
         json_response "Cannot find conversation for user", false, {conversations: []}, :not_found
         # json_response "Cannot find conversation for user", false, {conversations: []}, :not_found
@@ -114,7 +115,7 @@ class Api::V1::Users::ConversationsController < ApplicationController
       # p @conversation
       if @conversations
         if $redis
-          user_status_hash_array = get_status_of_conversation_users(@user.id, @conversations)
+          user_status_hash_array = get_set_status_of_conversation_users(@user.id, @conversations)
           p '**********in users, conversations_by_user, if flat id empty user_status_hash_array: ' + user_status_hash_array.to_s
         end
         conversations_serializer = parse_json @conversations
@@ -174,39 +175,48 @@ class Api::V1::Users::ConversationsController < ApplicationController
       json_response "Invalid token", false, {}, :failure
     end
   end
+  # Deprecated 
+  # def set_status_of_conversation_users(user_id, conversations)
+  #   conversations.each do |conv|
+  #     if conv.user_id != user_id
+  #       flat_owner_id = Flat.find_by(id: conv.flat_id).user_id
+  #       user_status_hash = get_user_status_by_user_id(flat_owner_id)
+  #       result = set_last_user_activity({user_id: flat_owner_id, logged_in: false, online: false, keep_online_status: false}) if user_status_hash.blank?
+  #     else
+  #       result = set_last_user_activity({user_id: user_id, logged_in: false, online: false, keep_online_status: false})
+  #     end
+  #     break unless result
+  #   end # end each conversations
+  #   return true
+  # end
 
-  def set_status_of_conversation_users(user_id, conversations)
-    conversations.each do |conv|
-      if conv.user_id = user_id
-        flat_owner_id = Flat.find_by(id: conv.flat_id).user_id
-        result = set_last_user_activity({user_id: flat_owner_id, logged_in: false, online: false, keep_online_status: false})
-      else
-        result = set_last_user_activity({user_id: user_id, logged_in: false, online: false, keep_online_status: false})
-      end
-      break unless result
-    end # end each conversations
-    return true
-  end
-
-  def get_status_of_conversation_users(user_id, conversations)
-    flat_owner_ids = []
+  def get_set_status_of_conversation_users(user_id, conversations)
+    conversation_user_id_array = []
     user_status_hash_array = []
 
     conversations.each do |conv|
-      if conv.user_id == user_id
-        p '**********in users, get_status_of_conversation_users, conv: ' + conv.to_s
-        p '**********in users, get_status_of_conversation_users, conv.flat_id: ' + conv.flat_id.to_s
-        flat_owner_ids = Flat.where(id: conv.flat_id).pluck(:user_id)
+      if conv.user_id != user_id
+        p '**********in users, get_set_status_of_conversation_users, conv: ' + conv.to_s
+        p '**********in users, get_set_status_of_conversation_users, conv.flat_id: ' + conv.flat_id.to_s
+        flat_owner_user_id = Flat.find_by(id: conv.flat_id).user_id
+        conversation_user_id_array.push(flat_owner_user_id)
       else
-        flat_owner_ids.push(conv.user_id)
+        conversation_user_id_array.push(conv.user_id)
       end # end of if
     end # end of each
 
-    flat_owner_ids.each do |each_id|
+    p '**********in users, get_set_status_of_conversation_users, conversation_user_id_array: ' + conversation_user_id_array.to_s
+    conversation_user_id_array.each do |each_id|
       user_status_hash = get_user_status_by_user_id(each_id)
-      user_status_hash_array.push(user_status_hash) unless !user_status_hash
+      if user_status_hash.blank?
+        result = set_last_user_activity({user_id: each_id, logged_in: false, online: false, keep_online_status: false})
+        user_status_hash = get_user_status_by_user_id(each_id)
+        user_status_hash_array.push(user_status_hash) unless user_status_hash.blank?
+      else
+        user_status_hash_array.push(user_status_hash) unless user_status_hash.blank?
+      end
     end
     return user_status_hash_array
-  end # end of get_status_of_conversation_users
+  end # end of get_set_status_of_conversation_users
 
 end # end of Class
